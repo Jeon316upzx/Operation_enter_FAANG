ff29a991e309633144cc543dc8cdec519b9ecbce
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2021-04-20 18:28:14 +0100

    bubbler complete

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,14 +7,16 @@ Implementing Bubble sort
 def my_bubbler(nums):
     if len(nums) <= 1:
         return nums
-    counter = 0
-    for i in range(1, len(nums)):
-        if nums[counter] > nums[i]:
-            temp = nums[counter]
-            nums[counter] = nums[i]
-            nums[i] = temp
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - 1):
+            print(nums[j], nums[j+1], "old values")
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j+1] = temp
+            print(nums[j], nums[j+1], "new values")
 
     return nums
 
 
-print(my_bubbler([9, 3, 1, 0, 34]))
+print(my_bubbler([3, 4, 56, 23, 12, 9, 3, 1, 0, 34]))
